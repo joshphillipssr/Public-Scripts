3ec6503928028f59016fb4605c95bbb0efbaba17
joshphillipssr <josh@joshphillipssr.com>
2024-11-01 20:39:41 -0400

    Update

--- batch.ps1
+++ batch.ps1
@@ -3,3 +3,4 @@ Write-Output "Launching PowerShell Windows Update (pswu.ps1) install script..."
 Invoke-Expression "& { $(Invoke-RestMethod https://raw.githubusercontent.com/joshphillipssr/Public-Scripts/refs/heads/main/pswu.ps1) }"
 
 Write-Output "Launching Power Configuraiton Settings (power.ps1) script..."
+Invoke-Expression "& { $(Invoke-RestMethod https://raw.githubusercontent.com/joshphillipssr/Public-Scripts/refs/heads/main/power.ps1) }" 
