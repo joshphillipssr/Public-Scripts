91e669c5a5836b2476f477b1395eee58b1a4fd7f
joshphillipssr <josh@joshphillipssr.com>
2024-11-01 20:55:46 -0400

    Updates

--- batch.ps1
+++ batch.ps1
@@ -4,3 +4,6 @@ Invoke-Expression "& { $(Invoke-RestMethod https://raw.githubusercontent.com/jos
 
 Write-Output "Launching Power Configuraiton Settings (power.ps1) script..."
 Invoke-Expression "& { $(Invoke-RestMethod https://raw.githubusercontent.com/joshphillipssr/Public-Scripts/refs/heads/main/power.ps1) }" 
+
+Write-Output "Launching Dell Command Update (dcu.ps1) script..."
+Invoke-Expression "& { $(Invoke-RestMethod https://raw.githubusercontent.com/joshphillipssr/Dell-Command-Update/refs/heads/main/dcu.ps1) }" 
