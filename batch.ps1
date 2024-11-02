62850c829b97b330ee317bbf5e282e3a1e8cd072
joshphillipssr <josh@joshphillipssr.com>
2024-11-01 20:24:02 -0400

    Update

--- batch.ps1
+++ batch.ps1
@@ -1,1 +1,1 @@
-iex "& { $(https://raw.githubusercontent.com/joshphillipssr/Public-Scripts/refs/heads/main/pswu.ps1) }"
+iex "& { $(irm https://raw.githubusercontent.com/joshphillipssr/Public-Scripts/refs/heads/main/pswu.ps1) }"
